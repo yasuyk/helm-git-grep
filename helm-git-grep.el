736cc16bdd93245bd0509c03f50187ba51ea2768
Yasuyuki Oka <yasuyk@gmail.com>
2013-11-10 00:26:48 +0900

    Dump version to 0.2

--- helm-git-grep.el
+++ helm-git-grep.el
@@ -4,7 +4,7 @@
 
 ;; Author: mechairoi
 ;; Maintainer: Yasuyuki Oka <yasuyk@gmail.com>
-;; Version: 0.1
+;; Version: 0.2
 ;; URL: https://github.com/yasuyk/helm-git-grep
 ;; Package-Requires: ((helm "1.0"))
 ;; Keywords: helm, git
