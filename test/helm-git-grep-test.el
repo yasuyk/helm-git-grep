1c72bedcbda87175c2b4f02457d54f3662d665dd
Yasuyuki Oka <yasuyk@gmail.com>
2016-04-03 11:28:16 +0900

    Define customizable faces

--- test/helm-git-grep-test.el
+++ test/helm-git-grep-test.el
@@ -27,13 +27,16 @@
 ;;; Code:
 
 
+(eval-when-compile (require 'cl))
 (require 'helm-git-grep)
 (require 'ert)
 
 (defun should-equal (a b)
     (should (equal a b)))
+(defun should-not-equal (a b)
+    (should-not (equal a b)))
 
-(ert-deftest helm-git-grep-ert-showing-leading-and-trailing-lines-option ()
+(ert-deftest ert--helm-git-grep-showing-leading-and-trailing-lines-option ()
   (let ((helm-git-grep-showing-leading-and-trailing-lines t))
     (should-equal (helm-git-grep-showing-leading-and-trailing-lines-option) "-1"))
   (let ((helm-git-grep-showing-leading-and-trailing-lines t)
@@ -42,17 +45,21 @@
   (should-equal (helm-git-grep-showing-leading-and-trailing-lines-option) nil)
   (should-equal (helm-git-grep-showing-leading-and-trailing-lines-option t) ""))
 
-(ert-deftest helm-git-grep-ert-args ()
+(ert-deftest ert--helm-git-grep-rgs ()
   (should-equal (helm-git-grep-args nil)
                 '("--no-pager" "grep" "--full-name" "-n" "--no-color" "-i"))
   (let ((helm-git-grep-ignore-case nil))
      (should-equal (helm-git-grep-args nil)
                    '("--no-pager" "grep" "--full-name" "-n" "--no-color"))))
 
-
-;;-- command
-;; (defun helm-git-grep-git-string (&rest args)
-;; (defun helm-git-grep-get-top-dir ()
+(ert-deftest ert--helm-git-grep-highlight-match ()
+  (let* ((helm-input "defun")
+         (result (helm-git-grep-highlight-match "(defun abc())")))
+    (should-equal (get-text-property 0 'face result) nil)
+    (cl-loop for x from 1 to (length helm-input)
+             do (should-equal (get-text-property x 'face result) 'helm-git-grep-match))
+    (cl-loop for x from (1+ (length helm-input)) to  (length result)
+             do (should-equal (get-text-property x 'face result) nil))))
 
 
 (provide 'helm-git-grep-test)
